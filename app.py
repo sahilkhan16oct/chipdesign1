81a24b2086694de623177e8b6476e45d289efb51
SaqibArhan <saqibmohd1693@gmail.com>
2024-10-22 18:54:36 +0530

    home

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ system = platform.system()
 
 app.secret_key = 'your_secret_key'  # Needed for session management
 
+@app.route('/', methods=['GET'])
+def home():
+     return jsonify({"yes:this site is working"})
+
 # authentication url
 app.register_blueprint(auth_bp, url_prefix='/auth')
 
